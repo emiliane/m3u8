f22a0a6b2968aea7f5baea2ca893bd0cd396016c
Emilian <emiliane@users.noreply.github.com>
2025-04-07 18:24:56 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import subprocess
-import urllib.request
+import asyncio
+import shlex
 from datetime import datetime
+from subprocess import Popen, PIPE
 
 where = 'Dowload'
 
@@ -14,7 +16,7 @@ playlists = [
     'https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_7.m3u8',
     #eng
     'https://storage.sardius.media/archives/2153BA7C697A514/events/site_daa826D4F1/4/playlist_1.m3u8',
-    'https://storage.sardius.media/archives/2153BA7C697A514/events/site_daa826D4F1/4/playlist_4.m3u8',
+    'https://storage.sardius.media/archives/2153BA7C697A514/events/site_daa826D4F1/4/playlist_6.m3u8',
     'https://storage.sardius.media/archives/2153BA7C697A514/events/site_daa826D4F1/4/playlist_7.m3u8',
     ]
 
@@ -29,7 +31,7 @@ dt = datetime.now()
 ts = datetime.timestamp(dt)
 where = where + '/' + str(ts)
 
-def myrunsubprocess(command):
+def myrunsubproces(command):
     print('Run command:', command)
     try:
         subprocess.run(command, shell=True, check=True, text=True)
@@ -37,160 +39,101 @@ def myrunsubprocess(command):
     except:
         print("Something went wrong!")
 
-def downloadPlaylists(location, playlists):
+async def myrunsubprocesasync(command):
+    print('Run command:', command)
+    try:
+        await subprocess.run(command, shell=True, check=True, text=True)
+        return
+    except:
+        print("Something went wrong!")
+
+def commandForDownloadPlaylists(location, playlists):
+    curlcommands = []
     for playlist in playlists:
-        print(playlist)
+        print('Playlist:', playlist)
+        parts = playlist.rsplit('/', 3)
+        stream = parts[1]
         parts = playlist.rsplit('/', 1)
         address = parts[0]
         parts = parts[-1].rsplit('.', 1)
         name = parts[0]
         extension = parts[-1]
-        print(address, name, extension)
+        print(address, name, extension, '('+ stream +')')
 
-        tempfolder = location + '/Playlist/' + name
-        myrunsubprocess('mkdir -p ' + tempfolder)
-        myrunsubprocess('mkdir -p ' + tempfolder + '/' + extension)
+        tempfolder = location + '/Playlist/' + stream + '/' + name
+        myrunsubproces('mkdir -p ' + tempfolder)
+        myrunsubproces('mkdir -p ' + tempfolder + '/' + extension)
 
         playlist = 'curl ' + playlist + ' -o ' + tempfolder +'/' + name + '.' + extension
-        myrunsubprocess(playlist)
+        myrunsubproces(playlist)
 
         catfile = 'cat ' + tempfolder + '/' + name + '.' + extension + ' | grep ' + name + ' | sort -V | xargs -I "{}" echo "' + address + '/{} -o '+ tempfolder + '/{}" > ' + tempfolder + '/' +  extension + '.txt'
-        myrunsubprocess(catfile)
+        myrunsubproces(catfile)
 
         curlall = 'curl … $(cat ' + tempfolder + '/' + name + '.' + extension + ' | grep ' + name + ' | sort -V | xargs -I "{}" echo "' + address + '/{} -o '+ tempfolder + '/' + extension + '/{}")'
-        myrunsubprocess(curlall)
+        curlcommands.append(curlall)
 
-        return
-        
-        for x in range(rangestart, rangestop):
-            number = "{:05d}".format(x)
-
-            playlist = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/' + playlist + '_' + number + '.' + extension + ' -o ' + tempfolder +'/'+ number + '.ts'
-
-            myrunsubprocess(playlist)
-
-        file = 'ls '+ tempfolder + '/*.ts | sort -V > ' + tempfolder + '/' + extension + '.txt'
-
-        print(file)
-        myrunsubprocess(file)
-
-        ffmpegts = 'ffmpeg -y -f mpegts -i concatf:' + tempfolder + '/' + extension + '.txt -c copy ' + tempfolder + '/ts.mkv'
-        print (ffmpegts)
-        myrunsubprocess(ffmpegts)
-
-        deletetempfolder = 'rm -r ' + tempfolder
-        print(deletetempfolder)
-        myrunsubprocess(deletetempfolder)
-
-
-        tempfolder = location + '/' + str(rangestart) + '-' + str(rangestop - 1)
-        mkvfolder = location + '/mkv'
-        print(tempfolder, mkvfolder)
-        
-        myrunsubprocess('mkdir -p ' + tempfolder)
-        myrunsubprocess('mkdir -p ' + mkvfolder)
-        
-        for x in range(rangestart, rangestop):
-            number = "{:05d}".format(x)
-
-            curlts = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_1_' + number + '.ts -o ' + tempfolder +'/'+ number + '.ts'
-            curlaac = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_6_' + number + '.aac -o ' + tempfolder +'/'+ number + '.aac'
-
-            myrunsubprocess(curlts)
-            myrunsubprocess(curlaac)
-
-        filets = 'ls '+ tempfolder + '/*.ts | sort -V > ' + tempfolder + '/ts.txt'
-        fileaac = 'ls '+ tempfolder + '/*.aac | sort -V > ' + tempfolder + '/aac.txt'
-
-        print(filets)
-        myrunsubprocess(filets)
-        print(fileaac)
-        myrunsubprocess(fileaac)
-
-        ffmpegts = 'ffmpeg -y -f mpegts -i concatf:' + tempfolder + '/ts.txt -c copy ' + tempfolder + '/ts.mkv'
-        print (ffmpegts)
-        myrunsubprocess(ffmpegts)
-
-        ffmpegaac = 'ffmpeg -y -i concatf:' + tempfolder + '/aac.txt -c copy ' + tempfolder + '/aac.mkv'
-        print (ffmpegaac)
-        myrunsubprocess(ffmpegaac)
-
-        ffmpegvideo = 'ffmpeg -y -i ' + tempfolder + '/ts.mkv -i ' + tempfolder + '/aac.mkv -c copy ' + mkvfolder + '/video_' + str(rangestart) + '-' + str(rangestop - 1) + '.mkv'
-        print (ffmpegvideo)
-        myrunsubprocess(ffmpegvideo)
-
-        deletetempfolder = 'rm -r ' + tempfolder
-        print(deletetempfolder)
-        myrunsubprocess(deletetempfolder)
+    return curlcommands
+    
 
 def downloadAll(location, rangestart, rangestop, playlist, extension):
     tempfolder = location + '/All/' + str(rangestart) + '-' + str(rangestop - 1)
     mkvfolder = location + '/All/mkv'
     print(tempfolder, mkvfolder)
-    
-    myrunsubprocess('mkdir -p ' + tempfolder)
-    myrunsubprocess('mkdir -p ' + mkvfolder)
-    
+ 
+    myrunsubproces('mkdir -p ' + tempfolder)
+    myrunsubproces('mkdir -p ' + mkvfolder)
+ 
     for x in range(rangestart, rangestop):
         number = "{:05d}".format(x)
 
         curl = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/' + playlist + '_' + number + '.' + extension + ' -o ' + tempfolder +'/'+ number + '.ts'
 
-        myrunsubprocess(curl)
+        myrunsubproces(curl)
 
     file = 'ls '+ tempfolder + '/*.ts | sort -V > ' + tempfolder + '/' + extension + '.txt'
 
-    print(file)
-    myrunsubprocess(file)
+    myrunsubproces(file)
 
     ffmpegts = 'ffmpeg -y -f mpegts -i concatf:' + tempfolder + '/' + extension + '.txt -c copy ' + tempfolder + '/ts.mkv'
-    print (ffmpegts)
-    myrunsubprocess(ffmpegts)
+    myrunsubproces(ffmpegts)
 
     deletetempfolder = 'rm -r ' + tempfolder
-    print(deletetempfolder)
-    myrunsubprocess(deletetempfolder)
-
+    myrunsubproces(deletetempfolder)
 
     tempfolder = location + '/' + str(rangestart) + '-' + str(rangestop - 1)
     mkvfolder = location + '/mkv'
     print(tempfolder, mkvfolder)
-    
-    myrunsubprocess('mkdir -p ' + tempfolder)
-    myrunsubprocess('mkdir -p ' + mkvfolder)
-    
+ 
+    myrunsubproces('mkdir -p ' + tempfolder)
+    myrunsubproces('mkdir -p ' + mkvfolder)
+ 
     for x in range(rangestart, rangestop):
         number = "{:05d}".format(x)
 
         curlts = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_1_' + number + '.ts -o ' + tempfolder +'/'+ number + '.ts'
         curlaac = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_6_' + number + '.aac -o ' + tempfolder +'/'+ number + '.aac'
 
-        myrunsubprocess(curlts)
-        myrunsubprocess(curlaac)
+        myrunsubproces(curlts)
+        myrunsubproces(curlaac)
 
     filets = 'ls '+ tempfolder + '/*.ts | sort -V > ' + tempfolder + '/ts.txt'
     fileaac = 'ls '+ tempfolder + '/*.aac | sort -V > ' + tempfolder + '/aac.txt'
 
-    print(filets)
-    myrunsubprocess(filets)
-    print(fileaac)
-    myrunsubprocess(fileaac)
+    myrunsubproces(filets)
+    myrunsubproces(fileaac)
 
     ffmpegts = 'ffmpeg -y -f mpegts -i concatf:' + tempfolder + '/ts.txt -c copy ' + tempfolder + '/ts.mkv'
-    print (ffmpegts)
-    myrunsubprocess(ffmpegts)
+    myrunsubproces(ffmpegts)
 
     ffmpegaac = 'ffmpeg -y -i concatf:' + tempfolder + '/aac.txt -c copy ' + tempfolder + '/aac.mkv'
-    print (ffmpegaac)
-    myrunsubprocess(ffmpegaac)
+    myrunsubproces(ffmpegaac)
 
     ffmpegvideo = 'ffmpeg -y -i ' + tempfolder + '/ts.mkv -i ' + tempfolder + '/aac.mkv -c copy ' + mkvfolder + '/video_' + str(rangestart) + '-' + str(rangestop - 1) + '.mkv'
-    print (ffmpegvideo)
-    myrunsubprocess(ffmpegvideo)
+    myrunsubproces(ffmpegvideo)
 
     deletetempfolder = 'rm -r ' + tempfolder
-    print(deletetempfolder)
-    myrunsubprocess(deletetempfolder)
+    myrunsubproces(deletetempfolder)
 
 def downloadGroupedNew(location, rangestop, rangestart=0, step=5, clear=False):
     print('Download at' ,location, 'from', rangestart, 'to', rangestop, 'grouped by', step)
@@ -209,9 +152,9 @@ def downloadGroupedNew(location, rangestop, rangestart=0, step=5, clear=False):
 
         tempfolder = location + '/temp/' + str(a) + '-' + str(b - 1)
         mkvfolder = location + '/mkv'
-        
-        myrunsubprocess('mkdir -p ' + tempfolder)
-        myrunsubprocess('mkdir -p ' + mkvfolder)
+     
+        myrunsubproces('mkdir -p ' + tempfolder)
+        myrunsubproces('mkdir -p ' + mkvfolder)
 
         for x in range(a, b):
             if (x > rangestop):
@@ -223,27 +166,48 @@ def downloadGroupedNew(location, rangestop, rangestart=0, step=5, clear=False):
                 curlts = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_1_' + number + '.ts -o ' + tempfolder +'/'+ number + '.ts'
                 curlaac = 'curl https://storage.sardius.media/archives/2153BA7C697A514/events/site_DBF5334817/4/playlist_6_' + number + '.aac -o ' + tempfolder +'/'+ number + '.aac'
 
-                myrunsubprocess(curlts)
-                myrunsubprocess(curlaac)
+                myrunsubproces(curlts)
+                myrunsubproces(curlaac)
 
         filets = 'ls '+ tempfolder + '/*.ts | sort -V > ' + tempfolder + '/ts.txt'
         fileaac = 'ls '+ tempfolder + '/*.aac | sort -V > ' + tempfolder + '/aac.txt'
 
-        myrunsubprocess(filets)
-        myrunsubprocess(fileaac)
+        myrunsubproces(filets)
+        myrunsubproces(fileaac)
 
         ffmpegts = 'ffmpeg -y -f mpegts -i concatf:' + tempfolder + '/ts.txt -c copy ' + tempfolder + '/ts.mkv'
-        myrunsubprocess(ffmpegts)
+        myrunsubproces(ffmpegts)
 
         ffmpegaac = 'ffmpeg -y -i concatf:' + tempfolder + '/aac.txt -c copy ' + tempfolder + '/aac.mkv'
-        myrunsubprocess(ffmpegaac)
+        myrunsubproces(ffmpegaac)
 
         ffmpegvideo = 'ffmpeg -y -i ' + tempfolder + '/ts.mkv -i ' + tempfolder + '/aac.mkv -c copy ' + mkvfolder + '/video_' + str(rangestart) + '-' + str(rangestop - 1) + '.mkv'
-        myrunsubprocess(ffmpegvideo)
+        myrunsubproces(ffmpegvideo)
 
         deletetempfolder = 'rm -r ' + tempfolder
         if clear:
-            myrunsubprocess(deletetempfolder)
+            myrunsubproces(deletetempfolder)
 
 #downloadGroupedNew(where, rangestop, rangestart, step=setOfStep)
-downloadPlaylists(where, playlists)
+commands = commandForDownloadPlaylists(where, playlists)
+
+#loop = asyncio.new_event_loop()
+#asyncio.set_event_loop(loop)
+#tasks = []
+
+for command in commands:
+    print(command)
+    
+    #command = shlex.split(command)
+    #print(command)
+    #process = Popen(command, stdout=PIPE, stderr=PIPE)
+    #process = Popen(command)
+    #stdout, stderr = process.communicate()
+    
+    #task = loop.create_task(myrunsubprocesasync(command))
+    #tasks.append(task)
+    
+    myrunsubproces(command)
+
+#loop.run_until_complete(asyncio.wait(tasks))
+#loop.close()
